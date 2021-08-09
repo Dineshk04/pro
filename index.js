166b0504c3347813a61e8827216d24078df72554
Dinesh Kumar M M <59198176+Dineshk04@users.noreply.github.com>
2021-08-09 22:38:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var x = document.getElementById("i");
+var b = document.getElementById("button");
+b.addEventListener('click', function () {
+   
+    x.src = "https://i.imgur.com/iOeUBV7.png";
+})
+var b1 = document.getElementById("button1");
+b1.addEventListener('click', function () {
+   
+    x.src = "https://i.imgur.com/PTgQlim.png";
+})
+var b2 = document.getElementById("button2");
+b2.addEventListener('click', function () {
+    
+    x.src = "https://i.imgur.com/Mplj1YR.png";
+})
